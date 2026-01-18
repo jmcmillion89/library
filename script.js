09ff2c438b188ee2f43d72e2f3d0305ed723993f
Justin <jmcmillion89@icloud.com>
2026-01-18 17:46:15 -0500

    Add card styling

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook)
+  getLibrary()
 }
 
 function updateRead(index) {
@@ -32,8 +33,8 @@ function getLibrary() {
     const libraryDiv = document.querySelector('.library');
     libraryDiv.textContent = '';
     myLibrary.forEach((book, index) => {
-        let id = book.id
         const newDiv = document.createElement('div');
+        const btnDiv = document.createElement('div')
         const titleHeader = document.createElement('h3');
         const titleText = document.createElement('p')
         const authorHeader = document.createElement('h3');
@@ -64,23 +65,21 @@ function getLibrary() {
           updateRead(index)
         })
         newDiv.appendChild(readCheckBox)
+        btnDiv.classList = 'card-button'
         deleteBtn.textContent = "X"
         deleteBtn.addEventListener('click', () => {
-          removeBook(id)
+          removeBook(book.id)
         })
-        newDiv.appendChild(deleteBtn)
+        btnDiv.appendChild(deleteBtn)
+        newDiv.appendChild(btnDiv)
         libraryDiv.appendChild(newDiv)
-
-
     })
 }
 
 
-addBookToLibrary('test book 1', 'test author 1', 234, true)
-addBookToLibrary('test book 2', 'test author 2', 334, false)
-addBookToLibrary('test book 3', 'test author 3', 334, false)
-addBookToLibrary('test book 4', 'test author 4', 334, true)
-addBookToLibrary('test book 5', 'test author 5', 334, false)
-addBookToLibrary('test book 6', 'test author 6', 334, false)
+addBookToLibrary('To Kill a Mockingbird', 'Harper Lee', 323, false)
+addBookToLibrary('1984', 'George Orwell', 328, true)
+addBookToLibrary('The Lord of the Rings', 'J.R.R. Tolkien', 1216, true)
+addBookToLibrary('The Lion, the Witch and the Wardrobe', 'C.S. Lewis', 206, true)
 
 getLibrary()
